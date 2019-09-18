ab43f4c3eecd05153d6afdcc5202e999ea062dd1
kacper21galantowicz@gmail.com <kacper21galant@gmail.com>
2019-09-18 15:55:26 +0200

    dice

--- app.js
+++ app.js
@@ -9,8 +9,40 @@ GAME RULES:
 
 */
 
- var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer, dice;
 
- scores = [0,0];
- roundScore = 0;
- activePlayer = 0;
+scores = [0, 0];
+roundScore = 0;
+activePlayer = 0;
+
+document.querySelector('.dice').style.display = 'none';
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click', function () {
+
+    dice = Math.floor(Math.random() * 6) + 1;
+
+    var diceDom = document.querySelector('.dice');
+    diceDom.style.display = 'block';
+    diceDom.src = 'dice-' + dice + '.png';
+
+    if (dice !== 1) {
+        roundScore += dice;
+        document.getElementById('current-' + activePlayer).textContent = roundScore;
+    } else {
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
+
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+
+        document.querySelector('.dice').style.display = 'none';
+    }
+});
+
